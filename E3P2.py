7cb0b681fa36e3aa0a60b09e4b513e29b8854499
TripleA14 <56235340+TripleA14@users.noreply.github.com>
2019-10-06 22:50:28 +0200

    Add files via upload

---
+++ E3P2.py
@@ -0,0 +1,6 @@
+#Ejercicio 3 Practica 2: Adrian Arias
+a=int(input("Escribe un numero entero y te dire si es un numero par o no:\n"))
+if a % 2 == 0:
+    print (a ,"es un numero par.")
+else:
+    print (a ,"no es un numero par.")
