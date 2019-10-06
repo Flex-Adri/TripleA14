4238f808f7e8f49ddef1697cc47e00f8f489dc2d
TripleA14 <56235340+TripleA14@users.noreply.github.com>
2019-10-06 22:32:18 +0200

    Add files via upload

---
+++ E1P2.py
@@ -0,0 +1,5 @@
+#Ejercicio 1 Practica 2: Adrian Arias
+a=float(input("Escribe la base de tu triangulo en metros cuadrados:\n"))
+b=float(input("Escribe la altura de tu triangulo en metros cuadrados:\n"))
+A=float((a*b)/2)
+print ("El área del triangulo es:", A ,"metros cuadrados")
