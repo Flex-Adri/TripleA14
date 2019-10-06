50b4e312f130b841fd21e155faa6498a6906d0e6
TripleA14 <56235340+TripleA14@users.noreply.github.com>
2019-10-06 22:39:17 +0200

    Add files via upload

---
+++ E2P2.py
@@ -0,0 +1,4 @@
+#Ejercicio 2 Practica 2: Adrian Arias
+t=float(input("Escribe una temperatura en grados centigrados:\n"))
+F=float(t*(9/5)+32)
+print("La equivalencia en grados Farenheit es de", F ,"grados Farenheit.")
