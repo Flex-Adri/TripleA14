aa74c57e0d582825c77b8d7ad63cdc0466ec223c
TripleA14 <56235340+TripleA14@users.noreply.github.com>
2019-10-06 22:55:37 +0200

    Add files via upload

---
+++ E4P2.py
@@ -0,0 +1,9 @@
+#Ejercicio 4 Practica 2: Adrian Arias
+a=float(input("Introduce un numero\n"))
+b=float(input("Introduce otro numero\n"))
+if a>b:
+    print ("El primero es mayor que el segundo.")
+if a<b:
+    print ("El segundo es mayor que el primero.")
+if a==b:
+    print ("Los dos numeros tienen el mismo valor.")
